4c35d9d77f2824c9aed9981f8380251a49fafa5a
Dillip-J <dillipprem@gmail.com>
2026-04-20 10:16:07 +0530

    updates

--- js/config.js
+++ js/config.js
@@ -1,3 +1,17 @@
 // js/config.js
-let API_BASE = 'https://backend-depolyment-1.onrender.com';
- 
+let API_BASE;
+
+if (
+  window.location.hostname === "127.0.0.1" ||
+  window.location.hostname === "localhost"
+) {
+  // 💻 LOCAL MODE
+  API_BASE = "http://127.0.0.1:8000";
+  console.log("🔌 Connected to LOCAL Backend");
+} else {
+  // 🌍 LIVE MODE
+  API_BASE = "https://backend-depolyment-3.onrender.com";
+  console.log("☁️ Connected to LIVE Cloud Backend");
+}
+
+// 🚨 Do NOT use export default API_BASE here!
